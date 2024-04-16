bc4ed7f487e2e01ef76c0d2e2c88add62f216a9b
hker <hker@cwi.nl>
2024-04-16 14:37:46 +0200

    Add tangent function

--- src/numerics.jl
+++ src/numerics.jl
@@ -1,13 +1,25 @@
 export numderiv  # allows to call function without prefix "MyTangent.jl/"
+export tangent
 
 """
     numderiv(f, x₀, ε=1e-7)
 
-    Returns the appproximate derivative of ``f`` at ``x₀```using the forward difference with step size ``ε``, namely
+    Returns the appproximate derivative of ``f`` at ``x₀`` using the forward difference with step size ``ε``, namely
 
     `` f'(x_0) \\approx \\frac{f(x_0 + \\varepsilon) - f(x_0)}{\\varepsilon} ``
 """
 function numderiv(f, x₀, ε=1e-7)
     df = f(x₀ + ε) - f(x₀)
     return df / ε
+end
+
+""""
+    tangent(f,x₀,ε)
+
+    Returns tangent on function ``f`` at ``x₀`` with numerical approximation of the slope based on numderiv
+"""
+function tangent(f,x₀,ε=1e-7)
+    slope = numderiv(f,x₀,ε)
+    t(x) = slope*(x-x₀) + f(x₀)
+    return t
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -8,3 +8,11 @@ using Test
     @test numderiv(f, 3, 1) == 2
     @test numderiv(g, 2) ≈ 11 atol =0.01
 end
+
+@testset "Tangents" begin
+    f(x) = 2x + 1
+    g(x) = x^2
+
+    @test tangent(f,1)(2) ≈ f(2) atol 0.01
+    @test tangent(g,0)(3) ≈ 0 atol 0.1
+end
