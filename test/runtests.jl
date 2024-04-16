f4ddd39b9cd55b263ce10db71f12fac33669837c
hker <hker@cwi.nl>
2024-04-16 13:43:56 +0200

    Add tests on numderiv

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,9 @@ using MyTangent
 using Test
 
 @testset "MyTangent.jl" begin
-    # Write your tests here.
+    f(x) = 2x + 1
+    g(x) = x^3 - x
+
+    @test numderiv(f, 3, 1) == 2
+    @test numderiv(g, 2) ≈ 11 atol =0.01
 end
